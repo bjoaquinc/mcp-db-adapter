ff77c3cf71c558e2edc81c9a195d3e484dbb221f
Joaquin Coromina <bjoaquinc@gmail.com>
2025-05-26 17:04:26 -0400

    Updated how the engines are defined

--- src/engines/mysql.ts
+++ src/engines/mysql.ts
@@ -1,17 +1,19 @@
 import z from "zod";
 
 export const MySQLConfigSchema = z.object({
+  type: z.literal('mysql'),
   host: z.string(),
-  port: z.number().int().positive(),
+  port: z.number(),
   user: z.string(),
   password: z.string(),
   database: z.string()
 });
 
 export interface MySQLConfig {
-    host: string;
-    port: number;
-    user: string;
-    password: string;
-    database: string;
+  type: 'mysql';
+  host: string;
+  port: number;
+  user: string;
+  password: string;
+  database: string;
 }
--- src/engines/sqlite.ts
+++ src/engines/sqlite.ts
@@ -2,10 +2,12 @@ import z from "zod";
 
 export const SQLiteConfigSchema = z.object({
   file: z.string(),           // allow ':memory:'
-  readonly: z.boolean().optional()
+  readonly: z.boolean().optional(),
+  type: z.literal('sqlite'),
 });
 
 export interface SQLiteConfig {
-    file: string;          // absolute path or `:memory:`
-    readonly?: boolean;
+  file: string;          // absolute path or `:memory:`
+  readonly?: boolean;
+  type: 'sqlite';
 }
